5db5a894b6be4cea4e1c0a31eeffc1d07402c369
parin111 <parinstar55@gmail.com>
2023-02-21 15:57:59 +0530

    addea section 2

--- py7.py
+++ py7.py
@@ -4,3 +4,10 @@ bye""")
 word = "python"
 print("j" + word[1:])
 print(len(s))
+#part 2
+# s = "First line. \nSecond line."
+print("""hi 
+bye""")
+word = "python"
+print("j" + word[1:])
+print(len(s))
